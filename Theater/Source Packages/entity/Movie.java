4c2229a1f4ce584e2eb6fa7293ac69955e2d4597
Huong Nguyen <htn190008@utdallas.edu>
2021-07-25 22:16:10 -0500

    entity + ejb created

---
+++ Theater/Source Packages/entity/Movie.java
@@ -0,0 +1,246 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author alice
+ */
+@Entity
+@Table(name = "MOVIE")
+@NamedQueries({
+    @NamedQuery(name = "Movie.findAll", query = "SELECT m FROM Movie m"),
+    @NamedQuery(name = "Movie.findByMovieid", query = "SELECT m FROM Movie m WHERE m.movieid = :movieid"),
+    @NamedQuery(name = "Movie.findByTitile", query = "SELECT m FROM Movie m WHERE m.titile = :titile"),
+    @NamedQuery(name = "Movie.findByGenre", query = "SELECT m FROM Movie m WHERE m.genre = :genre"),
+    @NamedQuery(name = "Movie.findByDescription", query = "SELECT m FROM Movie m WHERE m.description = :description"),
+    @NamedQuery(name = "Movie.findByCasts", query = "SELECT m FROM Movie m WHERE m.casts = :casts"),
+    @NamedQuery(name = "Movie.findByDirector", query = "SELECT m FROM Movie m WHERE m.director = :director"),
+    @NamedQuery(name = "Movie.findByYearmake", query = "SELECT m FROM Movie m WHERE m.yearmake = :yearmake"),
+    @NamedQuery(name = "Movie.findByDuration", query = "SELECT m FROM Movie m WHERE m.duration = :duration"),
+    @NamedQuery(name = "Movie.findByPoster", query = "SELECT m FROM Movie m WHERE m.poster = :poster"),
+    @NamedQuery(name = "Movie.findByTime1", query = "SELECT m FROM Movie m WHERE m.time1 = :time1"),
+    @NamedQuery(name = "Movie.findByTime2", query = "SELECT m FROM Movie m WHERE m.time2 = :time2"),
+    @NamedQuery(name = "Movie.findByTime3", query = "SELECT m FROM Movie m WHERE m.time3 = :time3")})
+public class Movie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "MOVIEID")
+    private Integer movieid;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "TITILE")
+    private String titile;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "GENRE")
+    private String genre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 1000)
+    @Column(name = "DESCRIPTION")
+    private String description;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "CASTS")
+    private String casts;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "DIRECTOR")
+    private String director;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 4)
+    @Column(name = "YEARMAKE")
+    private String yearmake;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "DURATION")
+    private String duration;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "POSTER")
+    private String poster;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "TIME1")
+    private String time1;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "TIME2")
+    private String time2;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "TIME3")
+    private String time3;
+
+    public Movie() {
+    }
+
+    public Movie(Integer movieid) {
+        this.movieid = movieid;
+    }
+
+    public Movie(Integer movieid, String titile, String genre, String description, String casts, String director, String yearmake, String duration, String poster, String time1, String time2, String time3) {
+        this.movieid = movieid;
+        this.titile = titile;
+        this.genre = genre;
+        this.description = description;
+        this.casts = casts;
+        this.director = director;
+        this.yearmake = yearmake;
+        this.duration = duration;
+        this.poster = poster;
+        this.time1 = time1;
+        this.time2 = time2;
+        this.time3 = time3;
+    }
+
+    public Integer getMovieid() {
+        return movieid;
+    }
+
+    public void setMovieid(Integer movieid) {
+        this.movieid = movieid;
+    }
+
+    public String getTitile() {
+        return titile;
+    }
+
+    public void setTitile(String titile) {
+        this.titile = titile;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCasts() {
+        return casts;
+    }
+
+    public void setCasts(String casts) {
+        this.casts = casts;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getYearmake() {
+        return yearmake;
+    }
+
+    public void setYearmake(String yearmake) {
+        this.yearmake = yearmake;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getTime1() {
+        return time1;
+    }
+
+    public void setTime1(String time1) {
+        this.time1 = time1;
+    }
+
+    public String getTime2() {
+        return time2;
+    }
+
+    public void setTime2(String time2) {
+        this.time2 = time2;
+    }
+
+    public String getTime3() {
+        return time3;
+    }
+
+    public void setTime3(String time3) {
+        this.time3 = time3;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (movieid != null ? movieid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Movie)) {
+            return false;
+        }
+        Movie other = (Movie) object;
+        if ((this.movieid == null && other.movieid != null) || (this.movieid != null && !this.movieid.equals(other.movieid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Movie[ movieid=" + movieid + " ]";
+    }
+    
+}
---
+++ Theater/Source Packages/entity/Theater.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author alice
+ */
+@Entity
+@Table(name = "THEATER")
+@NamedQueries({
+    @NamedQuery(name = "Theater.findAll", query = "SELECT t FROM Theater t"),
+    @NamedQuery(name = "Theater.findByTheaterid", query = "SELECT t FROM Theater t WHERE t.theaterid = :theaterid"),
+    @NamedQuery(name = "Theater.findByTheatername", query = "SELECT t FROM Theater t WHERE t.theatername = :theatername"),
+    @NamedQuery(name = "Theater.findByAddress", query = "SELECT t FROM Theater t WHERE t.address = :address"),
+    @NamedQuery(name = "Theater.findByCity", query = "SELECT t FROM Theater t WHERE t.city = :city"),
+    @NamedQuery(name = "Theater.findByState", query = "SELECT t FROM Theater t WHERE t.state = :state"),
+    @NamedQuery(name = "Theater.findByZipcode", query = "SELECT t FROM Theater t WHERE t.zipcode = :zipcode")})
+public class Theater implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "THEATERID")
+    private Integer theaterid;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "THEATERNAME")
+    private String theatername;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "ADDRESS")
+    private String address;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "CITY")
+    private String city;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2)
+    @Column(name = "STATE")
+    private String state;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 5)
+    @Column(name = "ZIPCODE")
+    private String zipcode;
+
+    public Theater() {
+    }
+
+    public Theater(Integer theaterid) {
+        this.theaterid = theaterid;
+    }
+
+    public Theater(Integer theaterid, String theatername, String address, String city, String state, String zipcode) {
+        this.theaterid = theaterid;
+        this.theatername = theatername;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+    }
+
+    public Integer getTheaterid() {
+        return theaterid;
+    }
+
+    public void setTheaterid(Integer theaterid) {
+        this.theaterid = theaterid;
+    }
+
+    public String getTheatername() {
+        return theatername;
+    }
+
+    public void setTheatername(String theatername) {
+        this.theatername = theatername;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (theaterid != null ? theaterid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Theater)) {
+            return false;
+        }
+        Theater other = (Theater) object;
+        if ((this.theaterid == null && other.theaterid != null) || (this.theaterid != null && !this.theaterid.equals(other.theaterid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Theater[ theaterid=" + theaterid + " ]";
+    }
+    
+}
