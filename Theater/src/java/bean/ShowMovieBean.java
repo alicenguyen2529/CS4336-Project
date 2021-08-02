192cd5cf19468cf937980b4e79c4e7be4edac5b5
ThinhBNguyen <87791130+ThinhBNguyen@users.noreply.github.com>
2021-08-02 13:14:56 -0500

    tried connecting backend
    not working yet

--- Theater/src/java/bean/ShowMovieBean.java
+++ Theater/src/java/bean/ShowMovieBean.java
@@ -8,26 +8,80 @@ package bean;
 import ejb.TheaterEJB;
 import entity.Movie;
 import entity.Theater;
-import java.io.Serializable;
-import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author thinh
  */
 @Named(value = "showMovieBean")
-@RequestScoped
+@SessionScoped
 public class ShowMovieBean implements Serializable {
 
-    @EJB
+     @EJB
     private TheaterEJB theaterEJB;
     private Theater theater;
     private Movie movie;
+
+    //getter and setter
+    public Theater getTheater() {
+        return theater;
+    }
+
+    public void setTheater(Theater theater) {
+        this.theater = theater;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
     
     
+
+    //show movie by theaterID. TheaterID is a parameter that get passed from html page.
+    public String showMovie()
+    {
+        try{
+            FacesContext fc = FacesContext.getCurrentInstance();
+            Map<String,String> params = fc.getExternalContext().getRequestParameterMap();
+            int theaterid = Integer.parseInt(params.get("theaterid"));
+            theater = theaterEJB.getTheaterByID(theaterid);
+            return "movie.xhtml";
+        }
+        
+        catch(Exception e)
+        {
+            return "Error";
+        }
+                
+    }
+    
+    //get list of movie
+    public List<Movie> getMovieList()
+    {
+        if(theater != null)
+        {
+            return theaterEJB.getMovie(theater.getTheaterid());
+        }
+        else 
+            return null;
+    }
+    /**
+     * Creates a new instance of ShowMovieBean
+     */
     public ShowMovieBean() {
+        
+        
     }
     
 }
--- Theater/src/java/ejb/TheaterEJB.java
+++ Theater/src/java/ejb/TheaterEJB.java
@@ -5,7 +5,9 @@
  */
 package ejb;
 
+import entity.Movie;
 import entity.Theater;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
@@ -26,9 +28,27 @@ public class TheaterEJB {
     public void persist(Object object) {
         em.persist(object);
     }
+    
+        public List<Theater> findAllTheater()
+    {
+        return em.createNamedQuery("Theater.findAll", Theater.class).getResultList();
+    }
 
     public Theater getTheater(String zipcode) {
         return em.createNamedQuery("Theater.findByZipcode", Theater.class).setParameter("zipcode", zipcode).getSingleResult();
 
+    }
+        public Theater getTheaterByID(int theaterid)
+    {
+        return em.createNamedQuery("Theater.findByTheaterid", Theater.class).setParameter("theaterid", theaterid).getSingleResult();
+    }
+        public List<Movie> getMovie(int theaterID)
+    {
+        return em.createNamedQuery("Theater.findMovie", Movie.class).setParameter("theaterid", theaterID).getResultList();
+    }
+    
+        public Movie getMovieByID(int movieid)
+    {
+        return em.createNamedQuery("Movie.findByMovieid", Movie.class).setParameter("movieid", movieid).getSingleResult();
     }
 }
